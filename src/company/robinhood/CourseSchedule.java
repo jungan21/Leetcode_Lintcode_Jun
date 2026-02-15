da6203ee00f52d3586538a6e486b08cb24558a2e
Jun Gan <jungan21@gmail.com>
2026-02-15 12:55:06 -0500

    Update CourseSchedule.java
    Optimization

--- src/company/robinhood/CourseSchedule.java
+++ src/company/robinhood/CourseSchedule.java
@@ -12,131 +12,19 @@ import java.util.*;
 public class CourseSchedule {
 
     public static void main(String[] args) {
-        // int[][] A = { { 1, 0 }, { 2, 0 }, { 3, 1 }, { 3, 2 } };
-        int[][] A = {{5, 8}, {3, 5}, {1, 9}, {4, 5}, {0, 2},
-                {1, 9}, {7, 8}, {4, 9}};
-        // int[][] A = { { 0, 1 }, { 1, 0 } };
-
-        int[] results1 = new CourseSchedule().findOrder(10, A);
-        int[] results2 = new CourseSchedule().findOrderJun(10, A);
-
-        for (int r1: results1) {
-            System.out.println(r1);
-        }
-        System.out.println("=====================");
-        for (int r2: results2) {
-            System.out.println(r2);
-        }
+        int[][] A = { { 1, 0 }, { 2, 0 }, { 3, 1 }, { 3, 2 } };
+        int[] results2 = new CourseSchedule().findOrderJun(4, A);
     }
 
-
-    // https://www.jiuzhang.com/solution/course-schedule
-    // Jun最优就解
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (prerequisites == null) {
-            return true;
-        }
-
-        int len = prerequisites.length;
-        if (numCourses == 0 || len == 0) {
-            return true;
-        }
-        Map<Integer, List<Integer>> graphMap = new HashMap<>();
-        // track the in degrees for each course (each course is as node in graph):
-        int[] inDegrees = new int[numCourses];
-        for (int[] a : prerequisites) {
-            // [1,0]    a[0] 需要先完成，才能完成a[1], 所以以a[1] 构建链接表图
-            if (graphMap.containsKey(a[1])) {
-                graphMap.get(a[1]).add(a[0]);
-            } else {
-                List<Integer> depList = new ArrayList<>();
-                depList.add(a[0]);
-                graphMap.put(a[1], depList);
-            }
-            inDegrees[a[0]]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegrees[i] == 0) {
-                queue.offer(i);
-            }
-        }
-
-        int[] result = new int[numCourses];
-        int index = 0;
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            result[index++] = course;
-            if (graphMap.containsKey(course)) {
-                for (int neighbor : graphMap.get(course)) {
-                    inDegrees[neighbor]--;
-                    if (inDegrees[neighbor] == 0) {
-                        queue.offer(neighbor);
-                    }
-                }
-            }
-        }
-
-        return index == numCourses;
-
-    }
-
-
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-
-        Map<Integer, List<Integer>> graphMap = new HashMap<>();
-        // track the in degrees for each course (each course is as node in graph):
-        int[] inDegrees = new int[numCourses];
-        for (int[] a : prerequisites) {
-            // [1,0]    a[0] 需要先完成，才能完成a[1], 所以以a[1] 构建链接表图
-            if (graphMap.containsKey(a[1])) {
-                graphMap.get(a[1]).add(a[0]);
-            } else {
-                List<Integer> depList = new ArrayList<>();
-                depList.add(a[0]);
-                graphMap.put(a[1], depList);
-            }
-            inDegrees[a[0]]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegrees[i] == 0) {
-                queue.offer(i);
-            }
-        }
-
-        int[] result = new int[numCourses];
-        int index = 0;
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            result[index++] = course; // 等于  result[index] = course, index++ 两句，顺序不能乱
-
-            if (graphMap.containsKey(course)) {
-                for (int neighbor : graphMap.get(course)) {
-                    inDegrees[neighbor]--;
-                    if (inDegrees[neighbor] == 0) {
-                        queue.offer(neighbor);
-                    }
-                }
-            }
-        }
-
-        return index == numCourses ? result : new int[0];
-
-    }
-
+    // jiuzhang:: https://www.jiuzhang.com/solution/course-schedule
     public int[] findOrderJun(int numCourses, int[][] prerequisites) {
-
         Map<Integer, List<Integer>> graphMap = new HashMap<>();
-        // track the in degrees for each course (each course is as node in graph):
-        // int[] inDegrees = new int[numCourses];
-        Map<Integer, Integer> indegreesMap = new HashMap<>();
+        // int[] inDegrees = new int[numCourses];  
+        Map<Integer, Integer> indegreesMap = new HashMap<>(); // track the in degrees for each course 
 
         for (int[] a : prerequisites) {
-            // [1,0]    a[0] 需要先完成，才能完成a[1], 所以以a[1] 构建链接表图
-            if (graphMap.containsKey(a[1])) {
+            // a[1]->a[0] 先take a[1], 以a[1] 构建图 类似loadfactor服务调用链{A->B|C} 图：A=[B, C]
+            if (graphMap.containsKey(a[1])) {  
                 graphMap.get(a[1]).add(a[0]);
             } else {
                 List<Integer> depList = new ArrayList<>();
@@ -144,36 +32,25 @@ public class CourseSchedule {
                 graphMap.put(a[1], depList);
             }
             //inDegrees[a[0]]++;
-            // a[1] finish, then can take a[0]
+           // 依赖关系：a[1]-> a[0] 计算a[0]的入度. 类似loadfactor服务调用链 {A->B|C}  A=[B, C] 计算B, C的入度
            indegreesMap.put(a[0], indegreesMap.getOrDefault(a[0], 0) + 1);
-//            if(indegreesMap.containsKey(a[0])){
-//                indegreesMap.put(a[0], indegreesMap.get(a[0]) + 1);
-//            } else {
-//                indegreesMap.put(a[0], 0);
-//            }
+        }  // System.out.println("indegreesMap" + indegreesMap);
 
-        }
-
-        System.out.println("indegreesMap" + indegreesMap);
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++){
-            if(!indegreesMap.containsKey(i)) {
+        Queue<Integer> queue = new LinkedList<>();   
+        for (int i = 0; i < numCourses; i++){   // find Ingree == 0
+            if(!indegreesMap.containsKey(i)) {  // if (inDegrees[i] == 0) 
                 queue.offer(i);
             }
         }
 
-
-       // System.out.println("queue" + queue);
-        int[] result = new int[numCourses];
-        int index = 0;
+        int[] result = new int[numCourses];  int index = 0;
         while (!queue.isEmpty()) {
             int course = queue.poll();
             result[index++] = course; // 等于  result[index] = course, index++ 两句，顺序不能乱
 
             if (graphMap.containsKey(course)) {
                 for (int neighbor : graphMap.get(course)) {
-                    int updatedInDegree = indegreesMap.get(neighbor) - 1;
+                    int updatedInDegree = indegreesMap.get(neighbor) - 1; // inDegrees[neighbor]--;
                     indegreesMap.put(neighbor, updatedInDegree);
 
                     if (updatedInDegree == 0) {
